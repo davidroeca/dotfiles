80df522e6fa1f68083f7068b7c403a3c9d6a00e6
davidroeca <david.roeca@gmail.com>
2017-05-10 14:54:18 -0400

    add error code to existing path branch

--- automation/move_anacron.sh
+++ automation/move_anacron.sh
@@ -5,6 +5,7 @@ DESTINATION_PATH="$HOME/.anacron"
 if [ -e $DESTINATION_PATH ] || [ -L $DESTINATION_PATH ]
 then
   echo "${DESTINATION_PATH} already exists; please move it safely or delete it"
+  exit 1
 else
   ln -s $SOURCE_PATH $DESTINATION_PATH
   echo "ran 'ln -s $SOURCE_PATH ${DESTINATION_PATH}' successfully"
