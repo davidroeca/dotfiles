8a7cfe69e9fe15bfe34e015323c1e0ba777c627e
davidroeca <david.roeca@gmail.com>
2026-02-26 16:45:26 -0500

    Fix bedrock configs

--- home-work-bash/bedrock-claude.sh
+++ home-work-bash/bedrock-claude.sh
@@ -1,3 +1,4 @@
-export ANTHROPIC_DEFAULT_OPUS_MODEL='us.anthropic.claude-opus-4-6-v1'
-export ANTHROPIC_DEFAULT_SONNET_MODEL='us.anthropic.claude-sonnet-4-6'
-export ANTHROPIC_DEFAULT_HAIKU_MODEL='us.anthropic.claude-haiku-4-5-20251001-v1:0'
+export CLAUDE_CODE_USE_BEDROCK="1"
+export ANTHROPIC_DEFAULT_OPUS_MODEL='global.anthropic.claude-opus-4-6-v1'
+export ANTHROPIC_DEFAULT_SONNET_MODEL='global.anthropic.claude-sonnet-4-6'
+export ANTHROPIC_DEFAULT_HAIKU_MODEL='global.anthropic.claude-haiku-4-5-20251001-v1:0'
